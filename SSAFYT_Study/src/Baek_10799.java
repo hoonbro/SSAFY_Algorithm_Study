490c9ea352b8b0e49a1b71ab96fa313e436ad3c7
변지훈 <wlgns1588@naver.com>
2021-01-26 23:31:40 +0900

    쇠막대기 문제

---
+++ SSAFYT_Study/src/Baek_10799.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//쇠막대기
+public class Baek_10799 {
+	public static void main(String[] args)  throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int total = 0;
+		int bar =0;
+		
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == '(')
+				bar++;
+			
+			else {
+				bar--;
+				
+				if(s.charAt(i-1) == '(') {
+					total += bar;
+				}
+				
+				else
+					total++;
+			}
+		}
+		
+		System.out.println(total);
+	}
+}
