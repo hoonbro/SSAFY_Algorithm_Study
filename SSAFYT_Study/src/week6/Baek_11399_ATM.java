57c1866fdd223b08bda81f7c4c5dc4e6879910ac
변지훈 <wlgns1588@naver.com>
2021-02-27 01:01:46 +0900

    구현

---
+++ SSAFYT_Study/src/week6/Baek_11399_ATM.java
@@ -0,0 +1,38 @@
+package week6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baek_11399_ATM {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		int total = arr[0];
+		for(int i = 1; i < N; i++) {
+			arr[i] += arr[i-1];
+			total += arr[i];
+		}
+		sb.append(total);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
