1feeaccec99a161b14323f4f350a1853564b87d7
변지훈 <wlgns1588@naver.com>
2021-02-26 17:48:16 +0900

    dfs

--- SSAFYT_Study/src/week6/Baek_16953_AB.java
+++ SSAFYT_Study/src/week6/Baek_16953_AB.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baek_16953 {
+//dfs
+public class Baek_16953_AB {
 	static long A, B, ans = 987654321;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
