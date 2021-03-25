4540916321a85cfef09244ef11ace434559116e5
변지훈 <wlgns1588@naver.com>
2021-03-25 22:20:37 +0900

    평범한배낭

---
+++ SSAFYT_Study/week10/Baek_12865_평범한배낭/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+	static int N, K;
+	static Product[] products;
+	
+	static class Product {
+		int weight, value;
+
+		public Product(int weight, int value) {
+			this.weight = weight;
+			this.value = value;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		products = new Product[N];
+		
+		int w, v;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+			v = Integer.parseInt(st.nextToken());
+			products[i] = new Product(w, v);
+		}
+		
+		int[] dp = new int[K+1];
+		for(int i = 0; i < N; i++) {
+			for(int j = K; j-products[i].weight >= 0; j--) {
+				if(dp[j] < dp[j - products[i].weight] + products[i].value) {
+					dp[j] = dp[j - products[i].weight] + products[i].value;
+				}
+			}
+		}
+		System.out.println(dp[K]);
+	}
+}
