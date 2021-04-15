6029e24f08d6213d90cacb19906a60b369a9ad4e
변지훈 <wlgns1588@naver.com>
2021-04-15 23:03:55 +0900

    구현

---
+++ SSAFYT_Study/week13/추가적으로 푼 문제/SWEA_활주로_건설.java
@@ -0,0 +1,108 @@
+package week13.추가;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class SWEA_활주로_건설 {
+	static int T, N, X, ans;
+	static int[][] map, map2;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			X = Integer.parseInt(st.nextToken());
+			map = new int[N][N];
+			map2 = new int[N][N];
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					map2[j][i] = map[i][j];
+				}
+			}
+			ans = 0;
+			check(map);
+			check(map2);
+			sb.append("#").append(t).append(" ").append(ans).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	static void check(int[][] map) {
+		boolean flag;
+		boolean[] build;
+		for(int i = 0; i < N; i++) {
+			flag = true;
+			build = new boolean[N];
+			loop:
+			for(int j = 0; j < N-1; j++) {
+				if(map[i][j] != map[i][j+1]) {
+					if(map[i][j] == map[i][j+1] +1) { //내려감
+						for(int k = j+1; k < j+1+X; k++) {
+							if(k >= N || map[i][j+1] != map[i][k] || build[k]) {
+								flag = false;
+								break loop;
+							}else
+								build[k] = true;
+						}
+					}
+					else if(map[i][j] == map[i][j+1] -1) {//올라감
+						for(int k = j; k >= j+1-X; k--) {
+							if(k < 0 || map[i][j] != map[i][k] || build[k]) {
+								flag = false;
+								break loop;
+							}else
+								build[k] = true;
+						}
+					}else {
+						flag = false;
+						break loop;
+					}
+				}
+			}
+			if(flag) {
+				ans++;
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
