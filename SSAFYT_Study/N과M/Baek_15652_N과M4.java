b09dd4774ba1ebe4d0c77a9736c2969ac340df38
변지훈 <wlgns1588@naver.com>
2021-10-19 23:30:23 +0900

    중복 조합

---
+++ SSAFYT_Study/N과M/Baek_15652_N과M4.java
@@ -0,0 +1,38 @@
+package N과M;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek_15652_N과M4 {
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		combi(1, 0);
+		System.out.println(sb.toString());
+	}
+
+	static void combi(int idx, int cnt) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = idx; i <= N; i++) {
+			arr[cnt] = i;
+			combi(i, cnt + 1);
+		}
+	}
+}
