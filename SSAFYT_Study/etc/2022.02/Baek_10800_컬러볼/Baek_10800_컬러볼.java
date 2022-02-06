1ea2835e4696cc2f92874baf9cbec5788fe51396
unknown <wlgns1588@naver.com>
2022-02-06 18:20:16 +0900

    누적합

---
+++ SSAFYT_Study/etc/2022.02/Baek_10800_컬러볼/Baek_10800_컬러볼.java
@@ -0,0 +1,72 @@
+package etc._2022_02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baek_10800_컬러볼 {
+	static int N;
+	static Ball[] balls;
+	static int[] ans, sum;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		N= Integer.parseInt(br.readLine());
+		balls = new Ball[N];
+		ans = new int[N];
+		sum = new int[N+1];
+		int total = 0;
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int color = Integer.parseInt(st.nextToken());
+			int size = Integer.parseInt(st.nextToken());
+			
+			balls[i] = new Ball(i, color, size);
+		}
+		Arrays.sort(balls);
+		
+		int idx = 0;
+		int cnt = 0;
+		for(int i = 1; i < N; i++) {
+			while(balls[idx].size < balls[i].size) {
+				total += balls[idx].size;
+				sum[balls[idx].color] += balls[idx].size;
+				idx++;
+			}
+			ans[balls[i].idx] = total - sum[balls[i].color];
+		}
+		
+		for(int i = 0 ; i < N; i++) {
+			sb.append(ans[i]).append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	
+	static class Ball implements Comparable<Ball>{
+		int idx, color, size;
+
+		public Ball(int idx, int color, int size) {
+			this.idx = idx;
+			this.color = color;
+			this.size = size;
+		}
+
+		@Override
+		public int compareTo(Ball o) {
+			if(this.size == o.size)
+				return this.color - o.color;
+			return this.size - o.size;
+		}
+	}
+}
